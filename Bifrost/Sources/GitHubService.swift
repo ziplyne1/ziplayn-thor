09051b46a663d2a05f726e7d01dd4817410ca397
Pax Willoughby <kilter_waspish0l@icloud.com>
2026-09-25 14:16:21 -0700

    Begin writing GitHubService with octocat call

--- Bifrost/Sources/GitHubService.swift
+++ Bifrost/Sources/GitHubService.swift
@@ -1,4 +1,74 @@
 import Foundation
+
 #if canImport(FoundationNetworking)
     import FoundationNetworking
 #endif
+
+enum GitHubServiceError: Error {
+    case requestFailed(statusCode: Int)
+    case decodingFailed(Error)
+    case noAssetsFound
+}
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case patch = "PATCH"
+    case delete = "DELETE"
+}
+
+struct GitHubRepo {
+    let owner: String
+    let repo: String
+}
+
+struct GitHubService {
+    let token: String? = ProcessInfo.processInfo.environment["GITHUB_TOKEN"]
+    let session = URLSession.shared
+
+
+    func createRequest(
+        method: HTTPMethod,
+        endpoint: String,
+        headers: [String: String] = [:]
+    ) throws -> URLRequest {
+        guard let url = URL(string: "https://api.github.com\(endpoint)") else {
+            throw URLError(.badURL)
+        }
+
+        var request = URLRequest(url: url)
+        request.httpMethod = method.rawValue
+
+        for key in headers.keys {
+            request.addValue(headers[key]!, forHTTPHeaderField: key)
+        }
+
+        return request
+    }
+
+
+    func octocat() async -> String {
+        do {
+            let request = try createRequest(
+                method: .get,
+                endpoint: "/octocat",
+                headers: [
+                    "Accept": "application/vnd.github+json",
+                    "X-GitHub-Api-Version": "2022-11-28",
+                ]
+            )
+            let response = try await session.data(for: request)
+
+            return String(data: response.0, encoding: .utf8)
+                ?? "Couldn't read the data from the URLResponse!"
+        } catch {
+            return error.localizedDescription
+        }
+    }
+    
+    func findLatestReleaseId(for repo: GitHubRepo) -> String {
+        // todo
+        return ""
+    }
+}
--- Bifrost/Sources/main.swift
+++ Bifrost/Sources/main.swift
@@ -1,32 +1,47 @@
 import Foundation
 
-print("Hello, Bifrost")
+// MARK: - Setup
 
+// MARK: Variables
 let targetsData = try Data(contentsOf: URL(fileURLWithPath: "targets.json"))
 let targets = try JSONDecoder().decode([Target].self, from: targetsData)
 
 let manifestData = try Data(contentsOf: URL(fileURLWithPath: "manifest.json"))
 let manifest = try JSONDecoder().decode([ManifestEntry].self, from: manifestData)
 
+let service = GitHubService()
 
-for target in targets {
-    let id = target.id
-    let entry = manifest.first(where: { $0.id == id })
 
-    print("")
-    print("---------------")
-    print(id)
-    print("  \(target.owner)/\(target.repo)")
-    print("  \(target.filepattern)")
-    print("  ")
+// MARK: Functions
+func printJsonInfo() {
+    for target in targets {
+        let id = target.id
+        let entry = manifest.first(where: { $0.id == id })
 
-    if let entry = entry {
-        print("  \(entry.filename)")
-        print("  \(entry.hash)")
-        print("  \(entry.lastChecked)")
-        print("  \(entry.lastUpdated)")
-    } else {
-        print("Couldn't find manifest entry for '\(id)")
+        print("")
+        print("---------------")
+        print(id)
+        print("  • \(target.owner)/\(target.repo)")
+        print("  • \(target.filepattern)")
+        print("  ")
+
+        if let entry = entry {
+            print("  • \(entry.filename)")
+            print("  • \(entry.hash)")
+            print("  • \(entry.lastChecked)")
+            print("  • \(entry.lastUpdated)")
+        } else {
+            print("  Couldn't find manifest entry for '\(id)")
+        }
     }
-
 }
+
+
+// MARK: - Entry
+print("Hello, Bifrost")
+print("\n------------------------------\n")
+printJsonInfo()
+print("\n------------------------------\n")
+
+let octocat = await service.octocat()
+print(octocat)
